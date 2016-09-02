1e259b89e7bc58738b3a4634aba222a9ed068543
dewanc36 <adil6543@gmail.com>
2016-09-02 18:29:27 -0400

    Added on click listener.

--- app/src/main/java/codingmecahnics/com/hackathonpractice/MainActivity.java
+++ app/src/main/java/codingmecahnics/com/hackathonpractice/MainActivity.java
@@ -2,6 +2,8 @@ package codingmecahnics.com.hackathonpractice;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickButton(View v){
+        TextView text1 =(TextView) findViewById(R.id.textView2);
+        text1.setText("Don't click me");
     }
 }
