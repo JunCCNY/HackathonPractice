ff0ca877884eb938c1af51a700737d1b169debe1
dewanc36 <adil6543@gmail.com>
2016-09-02 19:27:12 -0400

    Next activity

--- app/src/main/java/codingmecahnics/com/hackathonpractice/MainActivity.java
+++ app/src/main/java/codingmecahnics/com/hackathonpractice/MainActivity.java
@@ -1,5 +1,6 @@
 package codingmecahnics.com.hackathonpractice;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -16,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
     public void onClickButton(View v){
         TextView text1 =(TextView) findViewById(R.id.textView2);
         text1.setText("Don't click me, i forbid you, added by indrajit");
+    }
+
+    public void goToNextActivity(View view) {
+        Intent intent = new Intent(this, SeconderyActiviry.class);
+        startActivity(intent);
     }
 }
