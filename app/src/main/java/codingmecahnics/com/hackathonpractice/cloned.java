71e3c1a8e230f80d72be0c24476f55742968e771
Indrajit <injit.gurung@gmail.com>
2016-09-05 23:00:59 -0400

    added branchfucn

--- app/src/main/java/codingmecahnics/com/hackathonpractice/cloned.java
+++ app/src/main/java/codingmecahnics/com/hackathonpractice/cloned.java
@@ -4,4 +4,11 @@ package codingmecahnics.com.hackathonpractice;
  * Created by indrajit on 9/5/16.
  */
 public class cloned {
+    public void firstaddedfunc(){
+
+    }
+
+    public void branchfunc(){
+        
+    }
 }
